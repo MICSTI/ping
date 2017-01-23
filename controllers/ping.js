7c3adf37c66669425e9fcb1bc9336a4aeea6c88e
MICSTI <stifter.michael@gmx.net>
2017-01-23 17:40:57 +0100

    Add ping class and tests

---
+++ controllers/ping.js
@@ -0,0 +1,73 @@
+/**
+ * Created by michael.stifter on 23.01.2017.
+ */
+var Ping = function() {
+
+};
+
+Ping.prototype.checkType = function(type, value) {
+    if (['string', 'number', 'boolean', 'object', 'undefined'].indexOf(type.toLocaleLowerCase()) < 0) {
+        throw new Error("Invalid option type '" + type + "'");
+    }
+
+    return typeof value === type.toLocaleLowerCase();
+};
+
+Ping.prototype.checkExact = function(exact, value) {
+    return exact === value;
+};
+
+Ping.prototype.checkRange = function(range, value) {
+    if (range.lower && !range.higher) {
+        return value >= range.lower;
+    } else if (!range.lower && range.higher) {
+        return value <= range.higher;
+    } else if (range.lower && range.higher) {
+        return value >= range.lower && value <= range.higher;
+    } else {
+        return false;
+    }
+};
+
+Ping.prototype.result = function(opts, value) {
+    if (typeof opts === 'undefined') {
+        throw new Error("Missing options parameter");
+    }
+
+    // array to store the partial results in
+    var resultArray = [];
+
+    // ------- TYPE -------
+    var type = opts.type || null;
+
+    if (type !== null) {
+        resultArray.push(this.checkType(type, value));
+    }
+
+    // ------- EXACT VALUE -------
+    var exact = opts.exact || null;
+
+    if (exact !== null) {
+        resultArray.push(this.checkExact(exact, value));
+    }
+
+    // ------- RANGE -------
+    var range = opts.range || null;
+
+    if (range !== null && (range.lower || range.higher)) {
+        resultArray.push(this.checkRange(range, value));
+    }
+
+    // ------- FINAL CHECK -------
+    // check for 'false' values in result array
+    var numberOfFalses = resultArray.filter(function(r) {
+        return r === false;
+    }).length
+
+    // return final true or false
+    return numberOfFalses === 0;
+};
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = Ping;
+}
--- test/mocha/tests.js
+++ test/mocha/tests.js
@@ -8,9 +8,90 @@ var expect = chai.expect;
 
 chai.use(chaiHttp);
 
+var Ping = require('../../controllers/ping');
+
 // dummy test
 describe('dummy test', function() {
     it('should be able to calculate', function() {
         expect(40 + 2).to.equal(42);
+    });
+});
+
+// ping result checker
+describe('ping', function() {
+    beforeEach(function() {
+        ping = new Ping();
+    });
+
+    it('is an object', function() {
+        expect(ping).to.be.an('object');
+    });
+
+    it('has a result method', function() {
+        expect(ping.result).to.be.a('function');
+    });
+
+    it('rejects result call without parameters', function() {
+        expect(ping.result).to.throw(Error);
+    });
+
+    it('rejects result call with invalid type parameter', function() {
+        expect(ping.result.bind(undefined, { type: 'Dolores' })).to.throw(Error);
+    });
+
+    it('correctly checks a string result', function() {
+        expect(ping.result({ type: 'String' }, 'Dolores')).to.be.true;
+    });
+
+    it('correctly checks a number result', function() {
+        expect(ping.result({ type: 'Number' }, 42.42)).to.be.true;
+    });
+
+    it('correctly checks an object result', function() {
+        expect(ping.result({ type: 'Object' }, { a: 'A' })).to.be.true;
+    });
+
+    it('correctly checks a boolean result', function() {
+        expect(ping.result({ type: 'Boolean' }, false)).to.be.true;
+    });
+
+    it('correctly checks an object result', function() {
+        expect(ping.result({ type: 'Object' }, { a: 'A' })).to.be.true;
+    });
+
+    it('correctly checks an undefined result', function() {
+        expect(ping.result({ type: 'undefined' })).to.be.true;
+        expect(ping.result({ type: 'undefined' }, undefined)).to.be.true;
+    });
+
+    it('correctly rejects a result with a wrong type', function() {
+        expect(ping.result({ type: 'String' }, 8)).to.be.false;
+    });
+
+    it('correctly rejects an undefined result value', function() {
+        expect(ping.result({ type: 'String' })).to.be.false;
+        expect(ping.result({ type: 'boolean' })).to.be.false;
+    });
+
+    it('correctly checks an exact value', function() {
+        expect(ping.result({ exact: 'Dolores' }, 'Dolores')).to.be.true;
+        expect(ping.result({ exact: 42 }, 42)).to.be.true;
+    });
+
+    it('correctly rejects an incorrect exact value', function() {
+        expect(ping.result({ exact: 'Dolores' }, 'Abernathy')).to.be.false;
+        expect(ping.result({ exact: 'Dolores' }, 8)).to.be.false;
+    });
+
+    it('correctly checks a range value', function() {
+        expect(ping.result({ range: { lower: 10 } }, 10)).to.be.true;
+        expect(ping.result({ range: { higher: 43 } }, 42)).to.be.true;
+        expect(ping.result({ range: { lower: 41, higher: 43 } }, 42)).to.be.true;
+    });
+
+    it('correctly rejects an incorrect range value', function() {
+        expect(ping.result({ range: { lower: 10 } }, 8)).to.be.false;
+        expect(ping.result({ range: { higher: 43 } }, 45)).to.be.false;
+        expect(ping.result({ range: { lower: 41, higher: 43 } }, 40)).to.be.false;
     });
 });
