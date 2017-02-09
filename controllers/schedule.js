614318ae1a634f09c886732aca6ee089c4a5e0cf
MICSTI <stifter.michael@gmx.net>
2017-02-09 09:45:08 +0100

    Add basic requests for site monitoring

--- controllers/schedule.js
+++ controllers/schedule.js
@@ -7,6 +7,8 @@ var mailer = require('./mailer');
 
 var scheduleChecks = function() {
     schedule.scheduleJob('*/1 * * * *', function() {
+        winston.info('Started check');
+
         Site.find({
             active: true
         }, function(err, sites) {
@@ -17,7 +19,25 @@ var scheduleChecks = function() {
             }
 
             sites.forEach(function(site) {
+                // TODO check if site is propertly configured
+
                 // TODO perform check for site (if lastChecked and set interval are ok)
+
+                // perform request
+                request.get(site.config.url, function(error, response, body) {
+                    if (error) {
+                        // do something with error
+                        return winston.info('Site %s reponded with an error', site.name, {
+                            error: error,
+                            response: response,
+                            body: body
+                        });
+                    }
+
+                    var statusCode = response.statusCode;
+
+                    winston.info('Site %s responded with a status %d', site.name, statusCode);
+                });
 
                 // TODO update statistics for site object in database
             });
