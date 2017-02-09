15b53c1eb249e557caa90e6a6af0ee321003128d
MICSTI <stifter.michael@gmx.net>
2017-02-09 09:29:08 +0100

    Remove cluster code from boot script

--- boot.js
+++ boot.js
@@ -1,5 +1,3 @@
-var cluster = require("cluster");
-var numCpus = require("os").cpus().length;
 var schedule = require('./controllers/schedule');
 var logger = require('./log');
 
@@ -12,14 +10,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 // log environment
 logger.info('Environment is %s', process.env.NODE_ENV);
 
-// set up cluster
-/*cluster.setupMaster({ exec: __dirname + "/server.js" });
-
-for (var i = 0; i < numCpus; i++) {
-    cluster.fork();
-}*/
-
 // set up scheduled jobs
 schedule.scheduleChecks();
 
+// start server
 require('./server');
